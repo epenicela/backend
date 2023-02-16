a274fdf6558872ad7d98c84cf35119d18e703960
Elton Penicela <116995341+epenicela@users.noreply.github.com>
2023-02-16 14:17:31 +0200

    Update conexao.js

--- infaestrutura/conexao.js
+++ infaestrutura/conexao.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 
 const conexao = mysql.createConnection({
     host: "containers-us-west-101.railway.app",
-    port: 7238,
+    port: '7238',
     user: "root",
     password: "sGsTtMDCYOElyDTQIuR7",
     database: "railway"
