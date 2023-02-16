c4eb1c05fe0abea92db9eae567441b120c5073e1
Elton Penicela <116995341+epenicela@users.noreply.github.com>
2023-02-16 15:14:52 +0200

    Update conexao.js

--- infaestrutura/conexao.js
+++ infaestrutura/conexao.js
@@ -1,33 +1,12 @@
-/*const mysql = require("mysql");
+const mysql = require("mysql");
 
 
 const conexao = mysql.createConnection({
-    host: "containers-us-west-101.railway.app",
-    port: '7238',
-    user: "root",
-    password: "sGsTtMDCYOElyDTQIuR7",
-    database: "railway"
-});
-
-module.exports = conexao;*/
-
-
-//const mysql = require('mysql2')
-const mysql = require("mysql");
-
-const pool = mysql.createPool({
     host: process.env.DB_HOST, 
     user: process.env.DB_USERNAME, 
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    database: process.env.DB_DBNAME
 });
 
-pool.getConnection((err, conn) => {
-    if(err) console.log(err)
-    console.log("Connected successfully")
-})
-
-module.exports = pool.promise()
+module.exports = conexao;
+//port: '7238'
