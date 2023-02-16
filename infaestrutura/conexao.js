920500d9724061d943e6e8351af6fe86793a6d22
Elton Penicela <116995341+epenicela@users.noreply.github.com>
2023-02-16 03:15:44 +0200

    Update conexao.js

--- infaestrutura/conexao.js
+++ infaestrutura/conexao.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 
-var DB_HOST = process.env.DB_HOST || 'localhost';
+/*var DB_HOST = process.env.DB_HOST || 'localhost';
 var DB_USER = process.env.DB_USER || 'root';
 const DB_PASSWORD = process.env.DB_PASSWORD || '';
 var DB_NAME = process.env.DB_NAME || 'u868151405_pweb';
@@ -12,6 +12,16 @@ const conexao = mysql.createConnection({
     user: DB_USER,
     password: DB_PASSWORD,
     database: DB_NAME
+});*/
+
+
+
+const conexao = mysql.createConnection({
+    host: 'containers-us-west-101.railway.app',
+    port: 7238,
+    user: 'root',
+    password: 'sGsTtMDCYOElyDTQIuR7',
+    database: 'railway'
 });
 
 module.exports = conexao; 
